9996b14d8a8d3168ad5a1219fc2d024b5bcd2285
Charity Rymbai <charityrymbai@hotmail.com>
2024-11-02 11:55:35 +0530

    added func of profile page

--- apps/backend/src/Routes/dataRoutes.js
+++ apps/backend/src/Routes/dataRoutes.js
@@ -44,4 +44,101 @@ dataRouter.get("/getDashboardDetails", authMiddleware, async (c) => {
     }
 });
 
+dataRouter.get("/addlink", authMiddleware, async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+
+    try {
+        const projectDetails = await prisma.project.create({
+            data: {
+                user_id: c.user.user_id,
+                name: body.projectName,
+                url: body.githubLink,
+                description: body.description,
+            }
+        });
+
+        console.log(projectDetails);
+
+        const keywords = body.keywords.split(",");  
+
+        const hashtags = await prisma.hashTags.createMany({
+            data: keywords.map((keyword) => ({
+                hash_tag: keyword.trim(),
+                project_id: projectDetails.project_id,
+                upload_id: 0, 
+            })),
+            skipDuplicates: true,
+        });
+        
+
+
+        return c.json({
+            details: {
+                name: userDetails.name,
+                college: userDetails.college,
+                branch: userDetails.branch,
+                major: userDetails.major,
+            }
+        });
+    } catch (error) {
+            console.error(error);
+            return c.json(
+                {
+                    message: `${error}`,
+                },
+                500
+        );
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
+dataRouter.post("/addProfile", authMiddleware, async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+
+    try {
+        const userDetails = await prisma.user.update({
+            where: {
+                user_id: c.user.user_id,
+            },
+            data: {
+                name: body.name,
+                college: body.college,
+                branch: body.branch,
+                major: body.major,
+            }
+        });
+
+        return c.json({
+            message: "Profile Updated",
+            details: {
+                name: userDetails.name,
+                college: userDetails.college,
+                branch: userDetails.branch,
+                major: userDetails.major,
+            }
+        });
+    } catch (error) {
+            console.error(error);
+            return c.json(
+                {
+                    message: `${error}`,
+                },
+                500
+        );
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
+
+
 export default dataRouter;
