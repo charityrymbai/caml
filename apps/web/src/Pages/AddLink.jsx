7af14b60808c4b28b84e0bab1bfcc5a1dad15560
Jishnu Duhan <duhanjishnu@gmail.com>
2024-11-02 09:34:44 +0530

    edit project page

--- apps/web/src/Pages/AddLink.jsx
+++ apps/web/src/Pages/AddLink.jsx
@@ -76,7 +76,7 @@ function GetLink() {
             value={formData.description}
             onChange={handleChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-            rows="4"
+            rows="3"
             required
           ></textarea>
         </div>
